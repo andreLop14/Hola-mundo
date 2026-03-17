f2b7a0314045b4eb277dcd8be9cc5c9e122dc7c5
Hanny <hannyalonzo_2001@hotmail.com>
2026-03-16 21:00:53 -0600

    Se agrega el proyecto

---
+++ Hola mundo.php
@@ -0,0 +1,9 @@
+<?php
+// Definición de variables
+$nombre_estudiante = "Andres Benjamin López Pérez";
+
+echo "<h1>Hola mundo, soy $nombre_estudiante</h1>";
+echo "<p>Este es un script de PHP ejecutándose en un entorno interpretado.</p>";
+
+// Referencia a la imagen generada
+echo '<img src="estudiante_ia.jpg" alt="Imagen generada por IA"
